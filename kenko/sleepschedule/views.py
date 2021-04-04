8b51a52366bf133d119399e3c34634f4c8a2c405
Milind Madhukar <m.milind15@gmail.com>
2021-04-04 14:21:16 +0530

    Frontend for Meditation app

--- kenko/sleepschedule/views.py
+++ kenko/sleepschedule/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def sleep_schedule(req):
-    return render(req, 'sleep-schedule/home.html')
+def sleep_schedule(request):
+    return render(request, 'sleep-schedule/home.html')
